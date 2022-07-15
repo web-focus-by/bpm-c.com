94da3fef804a3a22ea8e31db7a7232b09cdc666f
Web-Focus <76510993+Web-Focus@users.noreply.github.com>
2022-07-15 12:31:40 +0300

    start_coding

---
+++ src/components/hero.js
@@ -0,0 +1,26 @@
+import * as React from "react"
+import PropTypes from "prop-types"
+import { Link } from "gatsby"
+import "./main.css"
+import "./icons.css"
+import "./moduls.css"
+
+const Hero = ({ siteTitle }) => (
+    <div className="container">
+     <div className="hero">
+     <div className="hero__title">«BPM CLOUD» an IT company that clearly knows</div>
+     <div className="hero__subtitle">We know all about art of creating effective websites, using both classic techniques and the most innovative world-class approaches.</div>
+     <div className="hero__butn"><button className="button">View work<span className="arrow_black"></span></button></div>
+     </div>
+    </div>
+)
+
+Hero.propTypes = {
+  siteTitle: PropTypes.string, 
+}
+
+Hero.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Hero
---
+++ src/components/it_company.js
@@ -0,0 +1,28 @@
+import * as React from "react"
+import PropTypes from "prop-types"
+import { Link } from "gatsby"
+import "./main.css"
+import "./icons.css"
+import "./moduls.css"
+
+const ITCompany = ({ siteTitle }) => (
+  <div className="container">
+    <div className="it_company">
+     <div className="it_company__title title_62">IT company</div>
+     <div className="it_company__info">«BPM CLOUD» digital marketing agency clearly knows what tools used to increase your sales and achieve your goals. We know all about art of creating effective websites, using both classic techniques and the most innovative world-class approaches. Result is important for our partners, and our web studio is ready to achieve it by joint efforts!</div>
+    </div>
+  </div>
+)
+
+ITCompany.propTypes = {
+  siteTitle: PropTypes.string, 
+}
+
+ITCompany.defaultProps = {
+  siteTitle: ``,
+}
+
+export default ITCompany
+
+
+
