afd64b2c6c8d2aff4c3525e65c1edbea66acb9fa
FOCUS-3\user <Aliaksey.dvaretski91@gmail.com>
2022-07-18 18:26:34 +0300

    Balls animation was added.

--- src/components/hero.js
+++ src/components/hero.js
@@ -6,13 +6,16 @@ import "./icons.css"
 import "./moduls.css"
 
 const Hero = ({ siteTitle }) => (
-    <div className="container">
-     <div className="hero">
-     <div className="hero__title">«BPM CLOUD» an IT company that clearly knows</div>
-     <div className="hero__subtitle">We know all about art of creating effective websites, using both classic techniques and the most innovative world-class approaches.</div>
-     <div className="hero__butn"><button className="button">View work<span className="arrow_black"></span></button></div>
-     </div>
+  <div className="container">
+    <div className="hero">
+      <div className="hero__title">«BPM CLOUD» an IT company that clearly knows</div>
+      <div className="hero__subtitle">We know all about art of creating effective websites, using both classic techniques and the most innovative world-class approaches.</div>
+      <div className="hero__butn"><button className="button">View work<span className="arrow_black"></span></button></div>
     </div>
+    <div className="header_circle"></div>
+    <div className="header_circle-2"></div>
+    <div className="header_circle-3"></div>
+  </div>
 )
 
 Hero.propTypes = {
