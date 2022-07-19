daf1cda676f274a094d09a2a235949f238b5a3a5
Web-Focus <76510993+Web-Focus@users.noreply.github.com>
2022-07-19 09:50:50 +0300

    add blocks

--- src/components/bpm_cloud.js
+++ src/components/bpm_cloud.js
@@ -9,29 +9,18 @@ import "../components/styles/mixins.css"
 const BPMCloud = ({ siteTitle }) => (
   <div className="container">
     <div className="bpm_cloud margin_bottom_240">
-      <div className="bpm_cloud__title title_62">Process of projects</div>
-      <div className="bpm_cloud__subtitle">We divide development into stages according to the end result. We can run them in parallel to help speed up the launch of the product.</div>
-      <div className="bpm_cloud__list">
-      <div class="bpm_cloud_list_item">
-        <p>analytics</p><span className="puzzle"></span>
-            <div class="bpm_cloud_list_item">
-            <p>graphics</p><span className="pen"></span>
-                <div class="bpm_cloud_list_item">
-                <p>design</p><span className="dye"></span>
-                    <div class="bpm_cloud_list_item">
-                    <p>front-end</p><span className="front"></span>
-                        <div class="bpm_cloud_list_item">
-                        <p>back-end</p><span className="back "></span>
-                            <div class="bpm_cloud_list_item">
-                            <p>testing</p><span className="testing"></span>
-                                <div class="bpm_cloud_list_item"><p>launch</p><span className="star"></span></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+      <div className="bpm_cloud__title title_62">BPM CLOUD</div>
+      <div className="bpm_cloud__info">
+      <div className="bpm_cloud_block">
+        <div className="bpm_cloud_block_title">Comprehensive approach to internet marketing</div>
+        <div className="bpm_cloud_block_text">Our online marketing agency has comprehensive approach to Internet marketing, so representatives of service sector, trade, construction, real estate managers and large players in other business areas trust exactly to «BPM CLOUD». We use the latest company products for our clients only. With our agency, you will get the most out of online advertising and online promotion.</div>
+        </div>
+        <div className="bpm_cloud_block">
+        <div className="bpm_cloud_block_title">Dynamically developing digital marketing firm</div>
+        <div className="bpm_cloud_block_text">The Belarusian digital space is very multifaceted. We have provided impressive number of vectors for making efforts. With advancement of technology, «BPM CLOUD» has improved turnkey experience for you, rather than a disparate activity set. Our web marketing company works with all existing effective worldwide network promotion channels. We have access to secrets of the most effective combinations of steps, tools, techniques so that you attract desired category customers at a mutually beneficial price. «BPM CLOUD» never stops, but is always in innovative tools search in order to take our partners’ business to the top.</div>
         </div>
       </div>
+
     </div>
   </div>
 )
