985fb07e36d458f34410df51789f53034d7dffc0
Web-Focus <76510993+Web-Focus@users.noreply.github.com>
2022-07-27 17:31:57 +0300

    add style

--- src/components/blog.js
+++ src/components/blog.js
@@ -5,6 +5,8 @@ import "../components/styles/main.css"
 import "../components/styles/icons.css"
 import "../components/styles/modules.css"
 import "../components/styles/mixins.css"
+import { Swiper, SwiperSlide } from "swiper/react"
+import "swiper/css"
 import imgVr from "../images/vr.svg"
 import imgPointer from "../images/pointer.svg"
 import imgScreen from "../images/screen.svg"
@@ -25,74 +27,107 @@ const Blog = ({ siteTitle }) => (
         </div>
       </div>
       <div className="blog__products">
-        <div className="blog_products_block">
-          <div className="blog_products_block_pic">
-            <img src={imgVr} />
-          </div>
-          <div className="blog_products_block_list hash">
-            <ul className="hash__list">
-              <li className="hash_list_block">
-                <a href="#">#design</a>
-              </li>
-              <li className="hash_list_block">
-                <a href="#">#design</a>
-              </li>
-            </ul>
-            <div className="blog_products_block_list_date">12.10.2021</div>
-          </div>
-          <div className="blog_products_block_title">«BPM CLOUD»</div>
-        </div>
-        <div className="blog_products_block">
-          <div className="blog_products_block_pic">
-            <img src={imgPointer} />
-          </div>
-          <div className="blog_products_block_list hash">
-            <ul className="hash__list">
-              <li className="hash_list_block">
-                <a href="#">#design</a>
-              </li>
-              <li className="hash_list_block">
-                <a href="#">#design</a>
-              </li>
-            </ul>
-            <div className="blog_products_block_list_date">12.10.2021</div>
-          </div>
-          <div className="blog_products_block_title">«BPM CLOUD»</div>
-        </div>
-        <div className="blog_products_block">
-          <div className="blog_products_block_pic">
-            <img src={imgScreen} />
-          </div>
-          <div className="blog_products_block_list hash">
-            <ul className="hash__list">
-              <li className="hash_list_block">
-                <a href="#">#design</a>
-              </li>
-              <li className="hash_list_block">
-                <a href="#">#design</a>
-              </li>
-            </ul>
-            <div className="blog_products_block_list_date">12.10.2021</div>
-          </div>
-          <div className="blog_products_block_title">«BPM CLOUD»</div>
-        </div>
-        <div className="blog_products_block">
-          <div className="blog_products_block_pic">
-            <img src={imgHand} />
-          </div>
-          <div className="blog_products_block_list hash">
-            <ul className="hash__list">
-              <li className="hash_list_block">
-                <a href="#">#design</a>
-              </li>
-              <li className="hash_list_block">
-                <a href="#">#design</a>
-              </li>
-            </ul>
-            <div className="blog_products_block_list_date">12.10.2021</div>
-          </div>
-          <div className="blog_products_block_title">«BPM CLOUD»</div>
-        </div>
+        <Swiper
+          spaceBetween={20}
+          slidesPerView={"auto"}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={swiper => console.log(swiper)}
+          breakpoints={{
+            1920: {
+              width: 1920,
+              allowTouchMove: false,
+            },
+            1366: {
+              width: 1366,
+              allowTouchMove: false,
+            },
+            1025: {
+              width: 1025,
+              allowTouchMove: false,
+            },
+            1024: {
+              width: 1366,
+              allowTouchMove: true,
+            },
+          }}
+        >
+          <SwiperSlide>
+            <div className="blog_products_block">
+              <div className="blog_products_block_pic">
+                <img src={imgVr} />
+              </div>
+              <div className="blog_products_block_list hash">
+                <ul className="hash__list">
+                  <li className="hash_list_block">
+                    <a href="#">#design</a>
+                  </li>
+                  <li className="hash_list_block">
+                    <a href="#">#design</a>
+                  </li>
+                </ul>
+                <div className="blog_products_block_list_date">12.10.2021</div>
+              </div>
+              <div className="blog_products_block_title">«BPM CLOUD»</div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="blog_products_block">
+              <div className="blog_products_block_pic">
+                <img src={imgPointer} />
+              </div>
+              <div className="blog_products_block_list hash">
+                <ul className="hash__list">
+                  <li className="hash_list_block">
+                    <a href="#">#design</a>
+                  </li>
+                  <li className="hash_list_block">
+                    <a href="#">#design</a>
+                  </li>
+                </ul>
+                <div className="blog_products_block_list_date">12.10.2021</div>
+              </div>
+              <div className="blog_products_block_title">«BPM CLOUD»</div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="blog_products_block">
+              <div className="blog_products_block_pic">
+                <img src={imgScreen} />
+              </div>
+              <div className="blog_products_block_list hash">
+                <ul className="hash__list">
+                  <li className="hash_list_block">
+                    <a href="#">#design</a>
+                  </li>
+                  <li className="hash_list_block">
+                    <a href="#">#design</a>
+                  </li>
+                </ul>
+                <div className="blog_products_block_list_date">12.10.2021</div>
+              </div>
+              <div className="blog_products_block_title">«BPM CLOUD»</div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="blog_products_block">
+              <div className="blog_products_block_pic">
+                <img src={imgHand} />
+              </div>
+              <div className="blog_products_block_list hash">
+                <ul className="hash__list">
+                  <li className="hash_list_block">
+                    <a href="#">#design</a>
+                  </li>
+                  <li className="hash_list_block">
+                    <a href="#">#design</a>
+                  </li>
+                </ul>
+                <div className="blog_products_block_list_date">12.10.2021</div>
+              </div>
+              <div className="blog_products_block_title">«BPM CLOUD»</div>
+            </div>
+          </SwiperSlide>
+        </Swiper>
       </div>
     </div>
   </div>
