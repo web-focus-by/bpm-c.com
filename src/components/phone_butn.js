c3907ab636739184a1d06f1c4fde1a3b8993d33c
FOCUS-3\user <Aliaksey.dvaretski91@gmail.com>
2022-07-29 17:48:43 +0300

    bugfix

--- src/components/phone_butn.js
+++ src/components/phone_butn.js
@@ -13,7 +13,7 @@ import "../components/styles/media_375.css"
 const PhoneButn = ({onClick}) => (
   <div className="phone_butn">
     <div className="phone" onClick={onClick}></div>
-    <span className="phone_icon"></span>
+    <span className="phone_icon" onClick={onClick}></span>
   </div>
 )
 
