cc149e6dadc66a1b3b88821affb27bc28230a195
FOCUS-3\user <Aliaksey.dvaretski91@gmail.com>
2022-08-12 10:54:50 +0300

    dev menu

--- src/components/dropdown_services.js
+++ src/components/dropdown_services.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { useState } from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import "../components/styles/main.css"
@@ -11,34 +12,34 @@ import "../components/styles/media_1024.css"
 import "../components/styles/media_768.css"
 import "../components/styles/media_375.css"
 
-const DropdownServices = ({ siteTitle }) => (
-    <div className="dropdown_services">
+const DropdownServices = ({ isToggle, turnOffMenu }) => {
+  const data = [
+    "IT Outsourcing","ASO Mobile App Optimisation","IT Outstaffing","GameDev","IT Consulting",
+    "Web Development Services","Web App Development","Website modification","Mobile App Development","Digital marketing",
+    "Desktop App Development","PPC Advertasing","UX/UI Services","SMM","Testing and QA",
+    "Landing page development","Web application design","Marketplace development","UI/UX mobile app design"
+  ]
+  const [isOpenPoint, setOpenPoint] = useState(false, setOpenPoint);
+  const closeMenu = () =>{
+    setOpenPoint(!isOpenPoint);
+    turnOffMenu();
+  }
+  const resultData = data.map((value, index) => {
+    return <li key={index} onClick={ closeMenu }><a href="#">{value}</a></li>
+  })
+  if (isToggle) {
+  return (
+    <div className="dropdown_services" >
         <div className="dropdown_services__title">Services</div>
         <div className="dropdown_services__info">
             <ul>
-                <li><a href="#">IT Outsourcing</a></li>
-                <li><a href="#">ASO Mobile App Optimisation</a></li>
-                <li><a href="#">IT Outstaffing</a></li>
-                <li><a href="#">GameDev</a></li>
-                <li><a href="#">IT Consulting</a></li>
-                <li><a href="#">Web Development Services</a></li>
-                <li><a href="#">Web App Development</a></li>
-                <li><a href="#">Website modification</a></li>
-                <li><a href="#">Mobile App Development</a></li>
-                <li><a href="#">Digital marketing</a></li>
-                <li><a href="#">Desktop App Development</a></li>
-                <li><a href="#">PPC Advertasing</a></li>
-                <li><a href="#">UX/UI Services</a></li>
-                <li><a href="#">SMM</a></li>
-                <li><a href="#">Testing and QA</a></li>
-                <li><a href="#">Landing page development</a></li>
-                <li><a href="#">Web application design</a></li>
-                <li><a href="#">Marketplace development</a></li>
-                <li><a href="#">UI/UX mobile app design</a></li>
+                {resultData}
             </ul>
         </div>
     </div>
-)
+  )
+}
+}
 
 DropdownServices.propTypes = { 
   siteTitle: PropTypes.string,
