de42dd306c4cb9504e3bf7620fd54fc4d95efffd
FOCUS-3\user <Aliaksey.dvaretski91@gmail.com>
2022-09-16 17:13:07 +0300

    blog page has been created.

--- src/components/listOfNews/listOfNews.js
+++ src/components/listOfNews/listOfNews.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { useState, useEffect } from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import Moment from 'moment';
@@ -18,8 +19,45 @@ const ListOfNews = ({ posts }) => {
   if (typeof window !== 'undefined') {
     url =  new URL(window.location.href);
   }
+  const [selectedPosts, setSelectedPosts] = useState(posts);
 
-  const items = posts.map((post, index) => {
+  const filterByTag = (item) => {
+    return console.log(item.toLowerCase())
+  }
+
+  const filterByCategory = (item) => {
+    return console.log(item.toLowerCase())
+  }
+
+  const topics = posts.reduce((allTopics, post) => {
+    let temporaryRes = post.node.tags.nodes.reduce((res, tag)=>{
+      return [...res, tag.slug];
+    },[])
+    allTopics.push(temporaryRes);
+    return allTopics;
+  },[])
+  const unicTopics = [...new Set(topics.reduce((next,prev)=>next.concat(prev),[]))];
+  const tags = unicTopics.map((tag, index) => {
+    return (
+      <button id={ tag } key={ tag } className="button_item_tag" onClick={ () =>{filterByTag(tag)} }>{ tag }</button>
+    )
+  });
+
+  const  categories = posts.reduce((allCategories, post) => {
+    let temporaryRes = post.node.categories.nodes.reduce((res, tag)=>{
+      return [...res, tag.slug];
+    },[])
+    allCategories.push(temporaryRes);
+    return allCategories;
+  },[])
+  const unicCategories = [...new Set(categories.reduce((next,prev)=>next.concat(prev),[]))];
+  const category = unicCategories.map((val, i)=>{
+    return (
+      <button id={ val } key={ val } className="button_item_tag" onClick={ ()=>{filterByCategory(val)} }>{ val }</button>
+    )
+  })
+
+  const items = selectedPosts.map((post, index) => {
     let tags = [];
     if (post && post.node.tags) {
       tags = post.node.tags.nodes.map((tag, i) => {
@@ -62,9 +100,22 @@ const ListOfNews = ({ posts }) => {
           { items }
         </div>
         <div className="blogs__topics">
-          <div className="blogs_products_block">
+          <div className="blogs_topics_block">
             <div className="blogs_topics_block_list">
-              Topics
+              <div className="blogs_topics_block_list_category">
+                <div className="blogs_topics_block_title">
+                  Blog
+                </div>
+                <button id={ "All" } key={ "All" } className="button_item_tag" onClick={ () => { filterByTag("All") } }>{ "All" }</button>
+                 { category } 
+              </div>
+              <div className="blogs_topics_block_list_topics">
+                <div className="blogs_topics_block_title">
+                  Topics
+                </div>
+                <button id={ "All" } key={ "All" } className="button_item_tag" onClick={ () => { filterByTag("All") } }>{ "All" }</button>
+                 { tags } 
+              </div>
             </div>
           </div>
         </div>
