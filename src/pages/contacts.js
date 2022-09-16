a37960bcbeed96a6d4b36df8f26125102e5743b6
FOCUS-3\user <Aliaksey.dvaretski91@gmail.com>
2022-09-16 17:42:38 +0300

    name of page was changed

--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -2,14 +2,14 @@ import * as React from "react"
 import Layout from "../components/layout"
 import Hero from "../components/hero"
 
-const Contact = ({ location }) => {
+const Contacts = ({ location }) => {
   return (
     <>
       <Layout>
-        <Hero location={ location } crumbLabel="Contacts"></Hero>
+        
       </Layout>
     </>
   )
 }
 
-export default Contact
+export default Contacts
