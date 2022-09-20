0a5fd724ad2431857bb649d04b4438aea4c393a2
FOCUS-3\user <Aliaksey.dvaretski91@gmail.com>
2022-09-20 17:27:38 +0300

    contact page

--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -42,6 +42,10 @@ const Contacts = ({ location }) => {
     )
   });
 
+  const addItem = (item) => {
+    console.log(item);
+  }
+
   return (
     <>
       <Layout>
@@ -77,9 +81,71 @@ const Contacts = ({ location }) => {
                 </div>
               </div>
             </div>
-            <div className="contacts__contact_form">
-              <div className="contacts_type_form">
-                
+            
+            <div className="contacts__contacts_form margin_bottom_240">
+              <div className="contact_form">
+                <div className="contact_form__block">
+                  <div className="contact_form_block_buttons_title">
+                  Services interested in:
+                  </div>
+                  <div className="contact_form_block_buttons">
+                    <button id="development" onClick={ ()=>{ addItem('development') } } className="button_item_tag">+ Development</button>
+                    <button id="design" onClick={ ()=>{ addItem('design') } } className="button_item_tag">+ Design</button>
+                    <button id="seo" onClick={ ()=>{ addItem('seo') } } className="button_item_tag">+ SEO</button>
+                    <button id="ppc" onClick={ ()=>{ addItem('ppc') } } className="button_item_tag">+ PPC</button>
+                    <button id="copywriting" onClick={ ()=>{ addItem('copywriting') } } className="button_item_tag">+ Copywriting</button>
+                  </div>
+                  <div className="contact_form_block_wrapper">
+                    <form id="search-contact_form" action="#" method="POST">
+                      <div className="contact_form_line-wrapper">
+                        <input
+                          id="name" 
+                          type="text"
+                          autoComplete="off"
+                          name="user_name"
+                          className="contact_form_name input-yellow "
+                          required
+                        />
+                        <label>Name*</label>
+                      </div>
+
+                      <div className="contact_form_line-wrapper">
+                        <input
+                          type="tel"
+                          id="tel"
+                          autoComplete="off"
+                          name="user_phone"
+                          className="contact_form-phone input-phone contact_form_phone input-yellow"
+                          required
+                        />
+                        <label>Phone*</label>
+                      </div>
+
+                      <div className="contact_form_line-wrapper">
+                        <input
+                          type="text"
+                          id="mail"
+                          autoComplete="off"
+                          name="user_mail"
+                          className="contact_form-mail input-mail contact_form_mail input-yellow"
+                          required
+                        />
+                        <label>E-mail</label>
+                      </div>
+                      <div className="contact_form_block_send">
+                        <div>
+                          <button className="button_white">
+                            Send<span className="arrow_black"></span>
+                          </button>
+                        </div>
+                        <p>
+                          By pressing the button, I agree for the processing of personal
+                          data
+                        </p>
+                      </div>
+                    </form>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
