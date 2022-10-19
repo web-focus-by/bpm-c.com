3cf6fb1a826579319a2fd94451c05d6e4edc2122
FOCUS-3\user <Aliaksey.dvaretski91@gmail.com>
2022-10-19 11:21:05 +0300

    refactoring

--- src/templates/faq.js
+++ src/templates/faq.js
@@ -1,4 +1,6 @@
 import * as React from "react"
+import { graphql } from "gatsby"
+import { fractionContent } from "../utils/fractionContent"
 import Layout from "../components/layout"
 import Breadcrumbs from "../components/breadcrumbs/breadcrumbs"
 import Seo from "../components/seo"
@@ -12,7 +14,24 @@ import "../components/styles/media_1024.css"
 import "../components/styles/media_768.css"
 import "../components/styles/media_375.css"
 
-const Faq = ({ location }) => {
+const Faq = ({ location, data }) => {
+  const content = data && data.wpPage ? fractionContent(data.wpPage.content) : null;
+  const title = content[0].title ? content[0].title.replace(/<[^>]+>/g, '') : null;
+  const contentPage = content[0].content.map((item, index)=>{
+    if (index === 0) {
+      return (
+        <div className="hero__subtitle" style={{LineHeight: '27px', fontSize: '18px', color: '#5C5C5C'}}>
+          <div key={ index } dangerouslySetInnerHTML={{__html: item.replace(/<[^>]+>/g, '') }} style={{width: '100%'}}/>
+        </div>
+      )
+    } else if (index === 1) {
+      return (
+        <div className="hero__subtitle margin_bottom_240" style={{LineHeight: '26.4px', fontSize: '24px', color: '#1A1B1F'}}>
+          <div key={ index } dangerouslySetInnerHTML={{__html: item.replace(/<[^>]+>/g, '') }} style={{width: '100%'}}/>
+        </div>
+      )
+    }
+  })
   return (
     <>
       <Layout>
@@ -24,6 +43,7 @@ const Faq = ({ location }) => {
             <div className="hero__title title_62">
               FAQ<span className="faq"></span>
             </div>
+            { contentPage }
           </div>
           <div className="header_circle_yellow"></div>
           <div className="header_circle_pink"></div>
@@ -36,3 +56,20 @@ const Faq = ({ location }) => {
 }
 
 export default Faq;
+export const query = graphql`
+  query getFaqDataQuery ($slug: String) {
+    wpPage(slug: {eq: $slug}) {
+    id
+    uri
+    title
+    content
+    parentId
+    slug
+    featuredImage {
+      node {
+        id
+        mediaItemUrl
+      }
+    }
+  }
+}`
