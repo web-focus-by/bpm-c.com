c110f8c557c55d0478064edc9bd53f3eb5228f12
IrynaKrt <106380034+IrynaKrt@users.noreply.github.com>
2023-08-03 16:44:59 +0300

    test: test new script

--- src/components/scriptLD.js
+++ src/components/scriptLD.js
@@ -1,24 +1,40 @@
 import * as React from "react"
 
-let data = {
-  \"\ @context \"\ : \"\ https://schema.org \"\,
-  \"\ @type \"\ : \"\ Organization \"\,
-  \"\url\"\: \"\ https://bpm-c.com/ \"\,
-  \"\ logo\"\ : \"\ https://wp.bpm-c.com/wp-content/uploads/2022/08/logo.svg \"\,
-  \"\ name \"\ : \"\ BPM Cloud \"\,
-  \"\ email \"\ : \"\ hello@bpm-c.com \"\,
-  \"\ address \"\: {
+// let data = {
+// "@context": "https://schema.org",
+//   "@type": "Organization",
+//   "url" : "https://bpm-c.com/",
+//   "logo": "https://wp.bpm-c.com/wp-content/uploads/2022/08/logo.svg",
+//   "name" : "BPM Cloud",
+//   "email": "hello@bpm-c.com",
+//   "address": {
+//     "@type": "PostalAddress",
+//     "addressLocality": "Minsk, Belarus",
+//     "postalCode": "220013",
+//     "streetAddress": "Independence Avenue, 77"
+//     },
+//   "telephone" : "+ 1 929 547 9159"
+// };
+
+const ORG_SCHEMA = JSON.stringify({
+  "@context": "https://schema.org",
+  "@type": "Organization",
+  "url" : "https://bpm-c.com/",
+  "logo": "https://wp.bpm-c.com/wp-content/uploads/2022/08/logo.svg",
+  "name" : "BPM Cloud",
+  "email": "hello@bpm-c.com",
+  "address": {
     "@type": "PostalAddress",
     "addressLocality": "Minsk, Belarus",
     "postalCode": "220013",
     "streetAddress": "Independence Avenue, 77"
     },
   "telephone" : "+ 1 929 547 9159"
-};
+});
 
 const ScriptLD = () => {
     return (
-      <script type="application/ld+json">{JSON.stringify(data)}</script>
+      <script type='application/ld+json' dangerouslySetInnerHTML={ { __html: ORG_SCHEMA} } />
     )
 }
 
