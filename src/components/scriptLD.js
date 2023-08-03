4060df5b4bd046cd7bf39d8b38485e13610134ec
IrynaKrt <106380034+IrynaKrt@users.noreply.github.com>
2023-08-03 16:51:21 +0300

    test: test script

--- src/components/scriptLD.js
+++ src/components/scriptLD.js
@@ -29,7 +29,18 @@ const ORG_SCHEMA = JSON.stringify({
     "postalCode": "220013",
     "streetAddress": "Independence Avenue, 77"
     },
-  "telephone" : "+ 1 929 547 9159"
+  "contactPoint" : [
+    { "@type" : "ContactPoint",
+      "telephone" : "+ 1 929 547 9159",
+      "contactType" : "customer service",
+      "areaServed" : "US"
+    } , {
+      "@type" : "ContactPoint",
+      "telephone" : "+ 1 647 493 9093",
+      "contactType" : "customer service",
+      "areaServed" : "CA"
+    }
+  ]
 });
 
 const ScriptLD = () => {
