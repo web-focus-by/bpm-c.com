a4c6c4aaa03a94da05663553a474828713be9c76
IrynaKrt <rinakashi13@mail.ru>
2023-08-07 13:12:48 +0300

    fix: edit sub-content

--- src/components/serviceItemsTextContent/serviceItemsTextContent.js
+++ src/components/serviceItemsTextContent/serviceItemsTextContent.js
@@ -19,12 +19,12 @@ const ServiceItemsTextContent = ({ content }) => {
         <div className="result_web_design margin_bottom_240">
           <div className="result_web_design_choice">
             <h2
-              className="result_web_design_choice__title title_62"
+              className="result_web_design_choice__title title_62 service-item__subtitle"
               dangerouslySetInnerHTML={{ __html: title }}
             />
             <div className="result_web_design_choice__block">
               <div
-                className="result_web_design_choice_block_list font_18"
+                className="result_web_design_choice_block_list font_18 service-item__subtitle"
                 dangerouslySetInnerHTML={{ __html: content.content.join("") }}
               />
             </div>
