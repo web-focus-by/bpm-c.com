cfccd53a912299d711b698874492e4d4924592df
Focus13 <C:\Users\Focus13\AppData\Roaming\The Bat!>
2023-08-08 14:34:37 +0300

    test: test markup

--- src/components/serviceHowToWork/serviceHowToWork.js
+++ src/components/serviceHowToWork/serviceHowToWork.js
@@ -10,28 +10,22 @@ import "../../components/styles/media_1024.scss"
 import "../../components/styles/media_768.scss"
 import "../../components/styles/media_375.scss"
 import "../../components/resultsOfWebDesign/resultsOfWebDesign.scss"
-import "./serviceHowToWork.scss"
 
 const ServiceHowToWork = ({ content }) => {
   if (content) {
     const title = content.title.replace(/<[^>]+>/g, "")
-    const midArr = content.content.shift();
     return (
         <div className="container">
-        <div className="result_web_design margin_bottom_240">
-          <div className="result_web_design_choice service_how_to">
+        <div className="service_how_to margin_bottom_240">
+          <div className="result_web_design_choice">
             <h2
               className="result_web_design_choice__title title_62 service-item__subtitle"
               dangerouslySetInnerHTML={{ __html: title }}
             />
-            <div
-                className="result_web_design_choice_block_list font_18 engagement-item"
-                dangerouslySetInnerHTML={{ __html: content.content[0] }}
-            />
             <div className="result_web_design_choice__block">
               <div
                 className="result_web_design_choice_block_list font_18 engagement-item"
-                dangerouslySetInnerHTML={{ __html: midArr }}
+                dangerouslySetInnerHTML={{ __html: content.content[1] }}
               />
             </div>
           </div>
