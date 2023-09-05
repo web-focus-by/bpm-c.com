d1b081bffb278f847e43a101c52106274392b13e
IrynaKrt <106380034+IrynaKrt@users.noreply.github.com>
2023-09-05 14:26:55 +0300

    Revert "React-navigation"

--- src/pages/news.js
+++ src/pages/news.js
@@ -1,5 +1,4 @@
 import * as React from "react"
-import { Navigate } from "react-router-dom"
 import { useStaticQuery, graphql } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -59,10 +58,8 @@ const News = ({ location }) => {
   const allTags = PostsAndTags ? PostsAndTags.allWpTag.edges : []
   const allPosts = PostsAndTags ? PostsAndTags.allWpPost.edges : []
 
-  if(typeof window !== 'undefined' && window.location.pathname === '/news/') {
-    return(
-      <Navigate to='/blog/'/>
-    )
+  if(typeof window !== 'undefined') {
+    console.log(window.location.pathname);
   }
 
 
