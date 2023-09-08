926a495584252e559cc16a5e1f500551783d9e47
IrynaKrt <rinakashi13@mail.ru>
2023-09-08 11:35:51 +0300

    refactor: delete site-map

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,54 +64,54 @@ module.exports = {
         stripQueryString: true,
       },
     },
-    {
-      resolve: "gatsby-plugin-sitemap",
-      options: {
-        query: `
-        {
-          allSitePage {
-            nodes {
-              path
-            }
-          }
-          allWpContentNode(filter: {nodeType: {in: ["Post", "Page"]}}) {
-            nodes {
-              ... on WpPost {
-                uri
-                modifiedGmt
-              }
-              ... on WpPage {
-                uri
-                modifiedGmt
-              }
-            }
-          }
-        }
-      `,
-        resolveSiteUrl: () => siteUrl,
-        resolvePages: ({
-          allSitePage: { nodes: allPages },
-          allWpContentNode: { nodes: allWpNodes },
-        }) => {
-          const wpNodeMap = allWpNodes.reduce((acc, node) => {
-            const { uri } = node
-            acc[uri] = node
+    // {
+    //   resolve: "gatsby-plugin-sitemap",
+    //   options: {
+    //     query: `
+    //     {
+    //       allSitePage {
+    //         nodes {
+    //           path
+    //         }
+    //       }
+    //       allWpContentNode(filter: {nodeType: {in: ["Post", "Page"]}}) {
+    //         nodes {
+    //           ... on WpPost {
+    //             uri
+    //             modifiedGmt
+    //           }
+    //           ... on WpPage {
+    //             uri
+    //             modifiedGmt
+    //           }
+    //         }
+    //       }
+    //     }
+    //   `,
+    //     resolveSiteUrl: () => siteUrl,
+    //     resolvePages: ({
+    //       allSitePage: { nodes: allPages },
+    //       allWpContentNode: { nodes: allWpNodes },
+    //     }) => {
+    //       const wpNodeMap = allWpNodes.reduce((acc, node) => {
+    //         const { uri } = node
+    //         acc[uri] = node
 
-            return acc
-          }, {})
+    //         return acc
+    //       }, {})
 
-          return allPages.map(page => {
-            return { ...page, ...wpNodeMap[page.path] }
-          })
-        },
-        serialize: ({ path, modifiedGmt }) => {
-          return {
-            url: path,
-            lastmod: modifiedGmt,
-          }
-        },
-      },
-    },
+    //       return allPages.map(page => {
+    //         return { ...page, ...wpNodeMap[page.path] }
+    //       })
+    //     },
+    //     serialize: ({ path, modifiedGmt }) => {
+    //       return {
+    //         url: path,
+    //         lastmod: modifiedGmt,
+    //       }
+    //     },
+    //   },
+    // },
     {
       resolve: "gatsby-plugin-robots-txt",
       options: {
