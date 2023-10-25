6c3719192b18b2fd2d88d1ca47afc7b86c371977
IrynaKrt <rinakashi13@mail.ru>
2023-10-25 20:41:20 +0300

    feat; add the Hover

--- src/components/faqItem.js
+++ src/components/faqItem.js
@@ -1,14 +1,14 @@
 import React, { useRef } from "react";
 
-export const AccordionItem = ({ faqItem, onClick, isOpen, num }) => {
+export const AccordionItem = ({ faqItem, onClick, onMouseOver, onMouseLeave, isOpen, isHover, num, }) => {
   const itemRef = useRef(null);
 
   return (
-    <li className="accordion-item">
+    <li className={`accordion-item`} onMouseOver={() => onMouseOver()} onMouseLeave={() => onMouseLeave()}> 
       <button className={`accordion-header ${isOpen ? "active" : ""}`} onClick={() => onClick()}>
         <div className="number">{num}</div>
         <p>{faqItem.q}</p>
-        <div className={`accordion-arrow ${isOpen ? "active" : ""}`}><span class={`${isOpen ? "label-icon_white" : "label-icon"}`}></span></div>
+        <div className={`accordion-arrow ${isOpen ? "active" : ""}`}><span class={`${isOpen ? "label-icon_white" : isHover ? "label-icon_hover" : "label-icon"}`}></span></div>
       </button>
       <div
         className="accordion-collapse"
