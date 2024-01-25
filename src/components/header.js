9dbf1e8febe7a2ce870f16bde77bc360866ae402
IrynaKrt <rinakashi13@mail.ru>
2024-01-25 14:46:53 +0300

    refactor: order the stylefiles

--- src/components/header.js
+++ src/components/header.js
@@ -5,11 +5,7 @@ import { Link } from "gatsby"
 import "../components/styles/main.scss"
 import "../components/styles/icons.scss"
 import "../components/styles/mixins.scss"
-import "../components/styles/media_1920.scss"
-import "../components/styles/media_1366.scss"
-import "../components/styles/media_1024.scss"
-import "../components/styles/media_768.scss"
-import "../components/styles/media_375.scss"
+import "../components/styles/header.scss"
 
 const Header = ({
   turnOnMenu,
