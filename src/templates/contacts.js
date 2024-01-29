7fecf193280b2007e2952139f2afd58597d98746
IrynaKrt <rinakashi13@mail.ru>
2024-01-29 11:26:50 +0300

    refactor: order contact page

--- src/templates/contacts.js
+++ src/templates/contacts.js
@@ -11,11 +11,7 @@ import "../components/styles/main.scss"
 import "../components/styles/icons.scss"
 import "../components/styles/modules.scss"
 import "../components/styles/mixins.scss"
-import "../components/styles/media_1920.scss"
-import "../components/styles/media_1366.scss"
-import "../components/styles/media_1024.scss"
-import "../components/styles/media_768.scss"
-import "../components/styles/media_375.scss"
+import "../components/styles/contact-page.scss"
 
 const CONTACT_MUTATION = gql`
   mutation CreateSubmissionMutation(
